3fc584a37a7d5eb4133d27277f253f89ecc40c0e
olygood <olivier.huttmacher@gmail.com>
2019-10-28 14:58:23 +0100

    move snake avec key code code error 86

--- script.js
+++ script.js
@@ -21,68 +21,124 @@ window.onload = function()
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
         canvas.style.border = "2px solid red";
-        //on a créer le canvas mais on ne la pas attaché a notre page HTML
+       //relie le body au canvas
         document.body.appendChild(canvas);
-        // pour dessiner dans un canvas on a besoin de contex = CTX
-        // plusieur facon de dessiner dans un canvas ici en 2d 
         ctx = canvas.getContext('2d');
-         //création du serpent sniky et il prend un body qui est un tableau de X,[[]]? des carres dans un coprs
-        sniky = new Snake([ [6,4],[5,4],[4,4] ]);
-        canvas.style.backgroundColor = 'rgb(237,255,238)'; // ou HEX // hexa decimal: #EDFFEE
-    
+        canvas.style.backgroundColor = 'rgb(237,255,238)'; 
+        
+        /*creation du snake avec la fonction snacke et constructeur*/
+        sniky = new Snake([ [6,4],[5,4],[4,4],"right" ]);
         refreshCanvas();
     
-    
     }
-    
 
-    //amener le mouvement avec un temp et un rafrechisement de la page
+    /*-----------------------------------------------------------------------------------*/    
         function refreshCanvas()
         {
-    
-          
-   //efface le rectangle pour qu'il affiche sa nouvelle position
-            
             ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    //dessiner sniky
             sniky.advance();
             sniky.draw();
-    // refresh toute les seconde 
             setTimeout(refreshCanvas,delay);
-           
+            
         }
             
-            function drawBlock(ctx, position)
-            {
-                var x = position[0] * blockSize;
-                var y = position[1] * blockSize;
-                ctx.fillRect(x, y, blockSize, blockSize); 
-            }
+        function drawBlock(ctx, position)
+        {
+            var x = position[0] * blockSize;
+            var y = position[1] * blockSize;
+            ctx.fillRect(x, y, blockSize, blockSize); 
+        }
 
-            function Snake(body)
+ /*--------------------------------------------------------------------------------------------------*/
+        function Snake(body,direction)
+        {
+            this.body = body;
+            this.direction = direction;
+            this.draw = function()
+        {
+            ctx.save();
+            ctx.fillStyle = "#86CF3E";
+            for(var i= 0; i<this.body.length;i++)
             {
-                this.body = body;
-                this.draw = function()
+                drawBlock(ctx, this.body[i]);
+            }
+            ctx.restore();
+        };
+/*----------------------------------------------------------------------------------------------*/
+            this.advance = function()
+            {
+                var nextPosition = this.body[0].slice(); //extrait le premier elelment du nouveau tableau 
+                switch(this.direction)
                 {
-        //savez le ctx comme il était avant
-                ctx.save();
-                ctx.fillStyle = "#86CF3E";
-                for(var i= 0; i<this.body.length;i++)
-                {
-                    drawBlock(ctx, this.body[i]);
+                    case "left":
+                        nextPosition[0] -=1;
+                    breack;
+                    case "right":
+                        nextPosition[0] +=1;
+                    breack;
+                    case "down":
+                        nextPosition[1] +=1;
+                    breack;
+                    case "up":
+                        nextPosition[1] -=1;
+                    breack;
+                    default : 
+                    throw("erreur of direction line 88");
                 }
-            // enc omplement avec ctx.save il remet le contexte comme avant
-                ctx.restore();
-                };
-                
-                this.advance = function()
+                nextPosition[0] += 1;
+                this.body.unshift(nextPosition); //rajoute un nouveau contenu au debut du nouveau tableau
+                this.body.pop();   //supprime le dernier element et retourne cette élément
+            };
+/*----------------------------------------------------------------------------------------------------------*/
+            this.setDirection = function(newDirection)
+            {   
+                //les direction permise par les touche et la direction
+                var allowedirections;
+                switch(this.direction)
                 {
-                    var nextPosition = this.body[0].slice();
-                    nextPosition[0] += 1;
-                    this.body.unshift(nextPosition);
-                    this.body.pop(); 
+                    case "left":
+                    case "right":
+                            allowedirections = ["up","down"];
+                    breack;
+                        case "up":
+                        case "down":
+                            allowedirections = ["left","right"]
+                        breack;
+                        default:
+                            throw("erreur of direction line 107");
+                }
+                if(allowedirections.indexOf(newDirection)> -1)
+                {
+                    this.direction = newDirection;
                 }
             }
+        }
+        //quand l'utilisateur appui sur une touche de sont clavier
+     this.document.onkeydown = function()
+     {
+         // on va fixé  la direction en fonction de la touche que l'on appuie
+         // et il faut la passer au serpent
+         var key = e.keyCode;
+         var newDirection;
+         switch(key)
+         {
+            case 37 :
+                newDirection = "left";
+            break;
+            case 38 :
+                newDirection = "up";
+            break;
+            case 39 :
+                newDirection = "right";
+            break;
+            case 40 :
+                newDirection = "down";
+            breack;
+            default :
+            return;
+         }
+         sniky.setDirection(newDirection);
+     }
 }
 
 
